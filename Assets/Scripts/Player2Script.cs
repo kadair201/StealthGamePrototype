7053efeaddfc97fad5ce58f02906006985ebbfb7
Kirsty Adair <kirstyaadair@gmail.com>
2019-02-04 23:41:22 +0000

    Player movement

---
+++ Assets/Scripts/Player2Script.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Script : MonoBehaviour
+{
+    // Variables
+    public float movementSpeed = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * movementSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * movementSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * movementSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * movementSpeed);
+        }
+    }
+}
